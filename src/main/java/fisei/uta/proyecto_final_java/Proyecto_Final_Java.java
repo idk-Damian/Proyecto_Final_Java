dcb54c38cd5609c98ac785a75b59e5962d930032
Damian <dl7pablok@gmail.com>
2026-05-27 22:03:20 -0500

    Primer commit con la estructura base

---
+++ src/main/java/fisei/uta/proyecto_final_java/Proyecto_Final_Java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fisei.uta.proyecto_final_java;
+
+/**
+ *
+ * @author USER
+ */
+public class Proyecto_Final_Java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
